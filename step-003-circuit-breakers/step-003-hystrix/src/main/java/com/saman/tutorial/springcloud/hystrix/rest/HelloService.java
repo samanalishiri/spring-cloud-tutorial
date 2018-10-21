19905bc9bb4ec720a208a59491403c98c59cf381
Saman Alishiri Shahrbabak <samanalishiri@gmail.com>
2018-10-21 20:22:25 +0430

    add step 003 circuit breakers hystrix

---
+++ step-003-circuit-breakers/step-003-hystrix/src/main/java/com/saman/tutorial/springcloud/hystrix/rest/HelloService.java
@@ -0,0 +1,34 @@
+package com.saman.tutorial.springcloud.hystrix.rest;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloService {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "helloFallBack")
+    @RequestMapping("step003/hello")
+    public String hello() {
+        throw new RuntimeException();
+    }
+
+    public String helloFallBack() {
+        return "Call helloFallBack!";
+    }
+
+}
