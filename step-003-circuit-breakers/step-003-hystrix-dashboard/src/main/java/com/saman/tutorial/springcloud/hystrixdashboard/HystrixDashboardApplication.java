6e26fde8ccfb4f0eb2ab38881180d45175452084
Saman Alishiri Shahrbabak <samanalishiri@gmail.com>
2018-11-07 15:13:23 +0330

    add step 003 circuit breakers hystrix dashboard

---
+++ step-003-circuit-breakers/step-003-hystrix-dashboard/src/main/java/com/saman/tutorial/springcloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.saman.tutorial.springcloud.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
