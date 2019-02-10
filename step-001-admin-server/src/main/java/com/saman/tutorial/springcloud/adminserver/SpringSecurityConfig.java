6019c4f1b3061ebdee64e31772d3506df3590465
Saman Alishiri <samanalishiri@gmail.com>
2019-02-10 18:59:22 +0330

    config admin server config eureka server for authenticate client

--- step-001-admin-server/src/main/java/com/saman/tutorial/springcloud/adminserver/SpringSecurityConfig.java
+++ step-001-admin-server/src/main/java/com/saman/tutorial/springcloud/adminserver/SpringSecurityConfig.java
@@ -1,40 +1,46 @@
-package com.saman.tutorial.springcloud.eureka;
+package com.saman.tutorial.springcloud.adminserver;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-@EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
+
+    public SpringSecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        // @formatter:off
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .requiresChannel()
                 .anyRequest().requiresSecure()
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll()
-                .and()
-                .httpBasic()
-                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
                 .csrf().disable();
     }
+
 
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -43,8 +49,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        userDetailsManager.createUser(User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build());
-        userDetailsManager.createUser(User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER").build());
+        userDetailsManager.createUser(User.builder().username("client").password(passwordEncoder().encode("client")).roles("CLIENT").build());
         return userDetailsManager;
     }
 
--- step-002-register-service/step-002-service/src/main/java/com/saman/tutorial/springcloud/service/ServiceApplication.java
+++ step-002-register-service/step-002-service/src/main/java/com/saman/tutorial/springcloud/service/ServiceApplication.java
@@ -4,13 +4,14 @@ import com.netflix.discovery.DiscoveryClient;
 import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.net.URL;
-import java.security.NoSuchAlgorithmException;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceApplication {
 
